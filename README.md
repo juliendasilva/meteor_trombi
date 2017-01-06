e658e5f6708cc0e559199bff7eb1bb638bd9ccfb
julien da silva <juliends93220@gmail.com>
2017-01-06 23:56:29 +0100

    add readme.md and indent some files

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# meteor_trombi
+# METEOR TROMBINOSCOPE
+
+## How to use ? 
+- git clone https://github.com/juliendasilva/meteor_trombi.git
+- meteor npm i
+- open localhost:3000 in your favorite web browser
+
+## What's the goal? 
+You can now create and delete students from the interface, Add grades and see the class average
+All of this in live
--- client/templateJS/add_eleve.js
+++ client/templateJS/add_eleve.js
@@ -8,5 +8,12 @@ Template.modal.events({
     const age        = template.find("#age").value;
     const specialite = template.find("#specialite").value;
     eleves.insert({prenom: prenom, nom: nom, classe: classe, groupe: groupe, mail: mail, age: age, specialite})
+    template.find("#prenom").value     = null;
+    template.find("#nom").value        = null;
+    template.find("#classe").value     = null;
+    template.find("#groupe").value     = null;
+    template.find("#mail").value       = null;
+    template.find("#age").value        = null;
+    template.find("#specialite").value = null;
   }
 });
